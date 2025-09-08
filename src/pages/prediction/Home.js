148e7add4cd96f011df08900ad46d2047a179518
coresyntax0330 <juancarlos.king0330@gmail.com>
2025-09-08 03:13:22 +0100

    -Update Terminal Writting and real chart

--- src/pages/prediction/Home.js
+++ src/pages/prediction/Home.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Image from "next/image";
 
 // import style
 import styles from "@/assets/css/prediction/home.module.css";
 
-// import assets
-import ChartImg from "@/assets/images/chart.png";
+// import components
+import UFOChart from "@/components/UFOChart";
 
 const Home = () => {
   return (
     <div className={styles.main}>
-      <Image src={ChartImg} alt="chart" className={styles.chartImg} priority />
+      <UFOChart />
       <div className={styles.info}>
         <div className={styles.wrapper}>
           <div className={styles.name}>Wallet:</div>
