2ba03dbc641a661158016bd21302db79fbdc3211
coresyntax0330 <juancarlos.king0330@gmail.com>
2025-09-18 10:41:18 +0100

    -Update

--- src/pages/dashboard/MiningCore.js
+++ src/pages/dashboard/MiningCore.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
+import {
+  useAccount,
+  useReadContract,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from "wagmi";
 import Image from "next/image";
 import { useDispatch } from "react-redux";
+
+// import component
+import { useToast } from "@/components/ToastProvider";
 
 // redux slices
 import { pageSet } from "@/redux/slices/pageSlice";
@@ -9,10 +18,111 @@ import { pageSet } from "@/redux/slices/pageSlice";
 import styles from "@/assets/css/dashboard/miningcore.module.css";
 
 // import assets
-import SphereImg from "@/assets/images/sphere.gif";
+import Tier1Img from "@/assets/images/station/tier1_1.gif";
+import Tier2Img from "@/assets/images/station/tier2_1.gif";
+import Tier3Img from "@/assets/images/station/tier3_1.gif";
+import Tier4Img from "@/assets/images/station/tier4_1.gif";
+import Tier5Img from "@/assets/images/station/tier5_1.gif";
+
+// import contracts
+import {
+  abstractorTokenContractAddress,
+  stationContractAddress,
+} from "@/utils/contract";
+import { stationABI } from "@/utils/abis/station";
+import { abstractorTokenContractABI } from "@/utils/abis/abstractor";
 
 const MiningCore = () => {
   const dispatch = useDispatch();
+  const { showToast } = useToast();
+  const { address } = useAccount();
+
+  const {
+    data: stationInfo,
+    isSuccess,
+    refetch: stationInfoContractRefetch,
+  } = useReadContract({
+    address: stationContractAddress,
+    abi: stationABI,
+    functionName: "getStationInfo",
+    args: address ? [address] : undefined,
+  });
+
+  const [displayed, setDisplayed] = useState([]); // finished items
+  const [currentLine, setCurrentLine] = useState("");
+  const [lineIndex, setLineIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+  const [skipped, setSkipped] = useState(false);
+  const [ready, setReady] = useState(false);
+  const [stationTier, setStationTier] = useState(0);
+  const [upgradeLoading, setUpgradeLoading] = useState(false);
+  const [refetchedStatus, setRefetchedStatus] = useState(false);
+
+  // ✅ two separate write hooks
+  const {
+    writeContract: writeApprove,
+    data: approveHash,
+    isPending: isApprovePending,
+  } = useWriteContract();
+
+  const {
+    writeContract: writeUpgrade,
+    data: upgradeHash,
+    isPending: isUpgradePending,
+  } = useWriteContract();
+
+  // ✅ tx waiters
+  const { isSuccess: isApproveSuccess } = useWaitForTransactionReceipt({
+    hash: approveHash,
+  });
+  const { isSuccess: isUpgradeSuccess } = useWaitForTransactionReceipt({
+    hash: upgradeHash,
+  });
+
+  const handleApproveUFOToken = async () => {
+    if (stationInfo && Number(stationInfo[0]) > 0) {
+      console.log(stationTier, "--stationTier--");
+
+      try {
+        setUpgradeLoading(true);
+        writeApprove({
+          address: abstractorTokenContractAddress,
+          abi: abstractorTokenContractABI,
+          functionName: "approve",
+          args: [
+            stationContractAddress,
+            stationTier === 1
+              ? 10000000000000000000n
+              : stationTier === 2
+              ? 30000000000000000000n
+              : stationTier === 3
+              ? 80000000000000000000n
+              : 550000000000000000000n,
+          ],
+        });
+      } catch (err) {
+        console.error("Error Approving UFO Token:", err);
+        showToast("Error UFO Approving Token");
+        setUpgradeLoading(false);
+      }
+    } else {
+      showToast("Please purchase station first");
+    }
+  };
+
+  const handleUpgradeStation = async () => {
+    try {
+      writeUpgrade({
+        address: stationContractAddress,
+        abi: stationABI,
+        functionName: "upgradeStation",
+      });
+    } catch (err) {
+      console.error("Error Upgrading Station:", err);
+      showToast("Error while Upgrading Station");
+      setBuyLoading(false);
+    }
+  };
 
   // Ordered sequence of elements
   const elements = [
@@ -31,10 +141,25 @@ const MiningCore = () => {
     { type: "image" },
 
     {
-      type: "btn",
-      text: "> 1. Upgrade Module [250 UFO]",
-      action: () => dispatch(pageSet("alert")),
+      type: "upgradeBtn",
+      text: `${
+        stationTier === 5
+          ? "> 1. Upgraded Max"
+          : `> 1. Upgrade Module [${
+              stationTier === 1
+                ? "10"
+                : stationTier === 2
+                ? "30"
+                : stationTier === 3
+                ? "80"
+                : stationTier === 4
+                ? "550"
+                : "0"
+            } UFO]`
+      }`,
+      action: () => {},
     },
+
     {
       type: "btn",
       text: "> 2. Fleet Management",
@@ -48,17 +173,50 @@ const MiningCore = () => {
     },
   ];
 
-  const [displayed, setDisplayed] = useState([]); // finished items
-  const [currentLine, setCurrentLine] = useState("");
-  const [lineIndex, setLineIndex] = useState(0);
-  const [charIndex, setCharIndex] = useState(0);
-  const [skipped, setSkipped] = useState(false);
+  useEffect(() => {
+    if (isSuccess && stationInfo) {
+      if (refetchedStatus) {
+        console.log(stationInfo[0], "---station info---");
+
+        setStationTier(Number(stationInfo[0]));
+      } else {
+        setReady(true);
+
+        setDisplayed([]);
+        setCurrentLine("");
+        setCharIndex(0);
+        setLineIndex(0);
+
+        setStationTier(Number(stationInfo[0]));
+      }
+    }
+  }, [isSuccess, stationInfo, refetchedStatus]);
 
   useEffect(() => {
+    if (isApproveSuccess) {
+      showToast("UFO Token Approved!");
+      handleUpgradeStation();
+    }
+  }, [isApproveSuccess]);
+
+  // buy success → final success
+  useEffect(() => {
+    if (isUpgradeSuccess) {
+      showToast("Upgrade Station Success!");
+      setUpgradeLoading(false);
+      setRefetchedStatus(true);
+      stationInfoContractRefetch();
+    }
+  }, [isUpgradeSuccess]);
+
+  useEffect(() => {
+    if (!ready) return;
+
     if (skipped) return;
 
     if (lineIndex < elements.length) {
       const current = elements[lineIndex];
+
       if (current.type === "image") {
         // show image instantly
         setDisplayed((prev) => [...prev, current]);
@@ -80,7 +238,7 @@ const MiningCore = () => {
         setLineIndex((prev) => prev + 1);
       }
     }
-  }, [charIndex, lineIndex, skipped]);
+  }, [charIndex, lineIndex, skipped, ready]);
 
   useEffect(() => {
     let lastTap = 0;
@@ -116,6 +274,10 @@ const MiningCore = () => {
       window.removeEventListener("touchend", handleTouch);
     };
   }, [skipped, lineIndex]);
+
+  if (!ready) {
+    return <div className={styles.main}>Loading station info...</div>;
+  }
 
   return (
     <div className={styles.main}>
@@ -162,7 +324,17 @@ const MiningCore = () => {
       {displayed.find((el) => el.type === "image") && (
         <div className={styles.wrapper}>
           <Image
-            src={SphereImg}
+            src={
+              stationTier === 1
+                ? Tier1Img
+                : stationTier === 2
+                ? Tier2Img
+                : stationTier === 3
+                ? Tier3Img
+                : stationTier === 4
+                ? Tier4Img
+                : Tier5Img
+            }
             alt="ship"
             className={styles.shipImg}
             priority
@@ -174,6 +346,46 @@ const MiningCore = () => {
 
       {/* BUTTONS */}
       <div className={styles.btnGroup}>
+        {displayed
+          .filter((el) => el.type === "upgradeBtn")
+          .map((el, i) => (
+            <button
+              key={`upgradeBtn-${i}`}
+              type="button"
+              className={styles.btn}
+              onClick={() => handleApproveUFOToken()}
+              disabled={
+                stationTier === 5 ||
+                upgradeLoading ||
+                isApprovePending ||
+                isUpgradePending
+              }
+            >
+              {upgradeLoading
+                ? "> Loading..."
+                : `${
+                    stationTier === 5
+                      ? "> 1. Upgraded Max"
+                      : `> 1. Upgrade Module [${
+                          stationTier === 1
+                            ? "10"
+                            : stationTier === 2
+                            ? "30"
+                            : stationTier === 3
+                            ? "80"
+                            : stationTier === 4
+                            ? "550"
+                            : "0"
+                        } UFO]`
+                  }`}
+            </button>
+          ))}
+        {lineIndex < elements.length && elements[lineIndex].type === "btn" && (
+          <button type="button" className={styles.btn}>
+            {currentLine}
+          </button>
+        )}
+
         {displayed
           .filter((el) => el.type === "btn")
           .map((el, i) => (
